9372d0f03de47001b0cc021bf18aa9a3f12374b3
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2023-07-16 21:27:24 +0700

    plugin: user plugin

--- src/services/postgres/UserService.js
+++ src/services/postgres/UserService.js
@@ -2,6 +2,7 @@ const { Pool } = require('pg');
 const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class UserService {
   constructor() {
@@ -18,7 +19,6 @@ class UserService {
     };
 
     const result = await this.pool.query(query);
-
     return result.rows[0];
   }
 
@@ -33,6 +33,22 @@ class UserService {
     if (result.rows.length > 0) {
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
     }
+
+    return result.rows[0].id;
+  }
+
+  async getUserById(userId) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE id = $1',
+      values: [userId],
+    };
+    const result = await this.pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('User tidak ditemukan');
+    }
+
+    return result.rows[0];
   }
 }
 
