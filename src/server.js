2b2928416877a0ceae24fb9c1fd42a4488e4fcc9
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2023-07-24 08:21:13 +0700

    auth plugin - DONE

--- src/server.js
+++ src/server.js
@@ -14,10 +14,16 @@ const users = require('./api/users');
 const UsersService = require('./services/postgres/UserService');
 const UsersValidator = require('./validator/users');
 
+const authentications = require('./api/authentication');
+const AuthenticationsService = require('./services/postgres/AuthenticationService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
 const init = async () => {
   const songsService = new SongsService();
   const albumsService = new AlbumsService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -29,30 +35,35 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
+    }, {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    }, {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumValidator,
+      },
+    }, {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
     },
-  });
-
-  await server.register({
-    plugin: albums,
-    options: {
-      service: albumsService,
-      validator: AlbumValidator,
-    },
-  });
-
-  await server.register({
-    plugin: users,
-    options: {
-      service: usersService,
-      validator: UsersValidator,
-    },
-  });
-
+  ]);
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
