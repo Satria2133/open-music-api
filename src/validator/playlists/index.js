6896224d2c179b1dc15d608fea3cec67972ed420
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2023-07-24 21:52:03 +0700

    playlist on progress

---
+++ src/validator/playlists/index.js
@@ -0,0 +1,19 @@
+const InvariantError = require('../../exceptions/InvariantError');
+const { PlaylistPayloadSchema, addSongToPlaylistPayloadSchema } = require('./schema');
+
+const PlaylistsValidator = {
+  validatePlaylistPayload: (payload) => {
+    const validationResult = PlaylistPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validateAddSongToPlaylistPayload: (payload) => {
+    const validationResult = addSongToPlaylistPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = PlaylistsValidator;
