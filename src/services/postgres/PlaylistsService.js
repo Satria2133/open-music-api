3bab3e8b297d075262dd520921845c6e9673cabe
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2023-07-30 19:02:49 +0700

    service : PlaylistsService.js

---
+++ src/services/postgres/PlaylistsService.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-underscore-dangle */
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+// const NotFoundError = require('../../exceptions/NotFoundError');
+// const AuthorizationError = require('../../exceptions/AuthorizationError');
+
+class PlaylistsService {
+  constructor(collaborationsService) {
+    this._pool = new Pool();
+    this._collaborationsService = collaborationsService;
+  }
+
+  async addPlaylist({ name, owner }) {
+    const id = `playlist-${nanoid(16)}`;
+    const query = {
+      text: 'INSERT INTO playlists VALUES($1, $2, $3) RETURNING id',
+      values: [id, name, owner],
+    };
+    const result = await this._pool.query(query);
+    if (!result.rows[0].id) {
+      throw new InvariantError('Playlist gagal ditambahkan');
+    }
+    return result.rows[0].id;
+  }
+}
+
+module.exports = PlaylistsService;
